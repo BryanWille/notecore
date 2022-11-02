bc9680ab838bd2fe47d7bca3ec5b60c175ff2a7e
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-01 22:02:56 -0300

    roteiro5 feito

--- notecore/lib/bryan/pages/home_page.dart
+++ notecore/lib/bryan/pages/home_page.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:notecore/bryan/pages/anuncios_page.dart';
+import 'package:notecore/bryan/pages/dados_page.dart';
+import 'package:notecore/bryan/pages/favoritos_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Layout Modelo'),
-        centerTitle: true,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Layout Modelo'),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.announcement)),
+            Tab(icon: Icon(Icons.cloud)),
+            Tab(icon: Icon(Icons.favorite))
+          ]),
+        ),
+        body: TabBarView(
+          children: [
+            AnuncioPage(),
+            DadosPage(),
+            FavoritosPage(),
+          ],
+        ),
       ),
-      body: AnuncioPage(),
     );
   }
 }
