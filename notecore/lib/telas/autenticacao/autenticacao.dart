a236ee9bff7f3710e8ad2341bd84b8d5dc05cb3a
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-11 22:11:29 -0300

    atualizando commits

---
+++ notecore/lib/telas/autenticacao/autenticacao.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Autenticacao extends StatefulWidget {
+  const Autenticacao({super.key});
+
+  @override
+  State<Autenticacao> createState() => _AutenticacaoState();
+}
+
+class _AutenticacaoState extends State<Autenticacao> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Autenticação"),
+    );
+  }
+}
